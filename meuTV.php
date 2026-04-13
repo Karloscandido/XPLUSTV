be8c94639c9ea8ce8087f189cc81165fd759dc7e
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-13 19:59:53 -0400

    Add files via upload

---
+++ meuTV.php
@@ -0,0 +1,30 @@
+{
+  "user_info": {
+    "username": "161364",
+    "password": "245193",
+    "message": "null",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "null",
+    "is_trial": "0",
+    "active_cons": "0",
+    "max_connections": "200",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts",
+      "rtmp"
+    ]
+  },
+  "server_info": {
+    "xui": true,
+    "version": "1.5.5",
+    "revision": 2,
+    "url": "fontez.cc",
+    "port": "80",
+    "https_port": "443",
+    "server_protocol": "http",
+    "rtmp_port": "8880",
+    "timestamp_now": 1774892165,
+    "timezone": "America/Sao_Paulo"
+  }
+}
