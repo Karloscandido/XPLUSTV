ac896d8d9730544cf4c0a781f6bf64b42f767baf
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-13 21:06:29 -0400

    Add files via upload

---
+++ VOD DEADPOOL.php.py
@@ -0,0 +1,30 @@
+{
+  "user_info": {
+    "username": "472016273115",
+    "password": "614377484372",
+    "message": "Contate o administrador!",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "1787713199",
+    "is_trial": "0",
+    "active_cons": 0,
+    "max_connections": "100",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts",
+      "rtmp"
+    ]
+  },
+  "server_info": {
+    "xui": true,
+    "version": "1.5.5",
+    "revision": 2,
+    "url": "d4sa56as6a23.top",
+    "port": "80",
+    "https_port": "443",
+    "server_protocol": "http",
+    "rtmp_port": "8880",
+    "timestamp_now": 1776128638,
+    "timezone": "America/Sao_Paulo"
+  }
+}
