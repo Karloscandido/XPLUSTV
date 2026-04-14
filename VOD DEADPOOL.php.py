fd5e3cfdd821f818892f3c47e35d92658816bbba
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-13 21:18:47 -0400

    Update VOD DEADPOOL.php.py

--- VOD DEADPOOL.php.py
+++ VOD DEADPOOL.php.py
@@ -1,14 +1,14 @@
 {
   "user_info": {
-    "username": "472016273115",
-    "password": "614377484372",
-    "message": "Contate o administrador!",
+    "username": "897549710171459",
+    "password": "0999369741520",
+    "message": "Deus te abençoe sempre.",
     "auth": 1,
     "status": "Active",
-    "exp_date": "1787713199",
+    "exp_date": "1792202238",
     "is_trial": "0",
-    "active_cons": 0,
-    "max_connections": "100",
+    "active_cons": "0",
+    "max_connections": "10",
     "allowed_output_formats": [
       "m3u8",
       "ts",
@@ -19,12 +19,12 @@
     "xui": true,
     "version": "1.5.5",
     "revision": 2,
-    "url": "d4sa56as6a23.top",
+    "url": "beststre.com",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
-    "timestamp_now": 1776128638,
+    "timestamp_now": 1776129374,
     "timezone": "America/Sao_Paulo"
   }
 }
