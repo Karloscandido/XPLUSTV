9d4efb46b06b249558fa7d8e1e40cfafde4f98a6
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-16 21:15:41 -0400

    Update VOD DEADPOOL.php.py

--- VOD DEADPOOL.php.py
+++ VOD DEADPOOL.php.py
@@ -1,39 +1,30 @@
 {
   "user_info": {
-    "username": "TouroVod-qEPnxNaSV5",
-    "password": "FMrPn9U4lJ",
-    "username": "TouroVod-qEPnxNaSV5",
-    "password": "FMrPn9U4lJ",
-    "username": "TouroVod-qEPnxNaSV5",
-    "password": "FMrPn9U4lJ",
-    "username": "TouroVod-qEPnxNaSV5",
-    "password": "FMrPn9U4lJ",
-    "username": "TouroVod-qEPnxNaSV5",
-    "password": "FMrPn9U4lJ",
-    "message": "Welcome to My Cine",
+    "username": "950031",
+    "password": "ccwWkn",
+    "message": null,
     "auth": 1,
     "status": "Active",
-    "exp_date": "2034150075",
+    "exp_date": null,
     "is_trial": "0",
-    "active_cons": "0",
-    "max_connections": "10",
+    "active_cons": 1,
+    "created_at": "1567789797",
+    "max_connections": "5",
     "allowed_output_formats": [
       "m3u8",
-      "ts",
-      "rtmp"
+      "ts"
     ]
   },
   "server_info": {
-    "url": "mcp7xv2p.fifidobisowiba.info",
-    "url": "mcp7xv2p.fifidobisowiba.info",
-    "url": "mcp7xv2p.fifidobisowiba.info",
-    "url": "mcp7xv2p.fifidobisowiba.info",
-    "url": "mcp7xv2p.fifidobisowiba.info",
-    "port": "2052",
-    "https_port": "2096",
+    "xui": true,
+    "version": "1.5.5",
+    "revision": 2,
+    "url": "mix34.ddns.net",
+    "port": "80",
+    "https_port": "443",
     "server_protocol": "http",
-    "rtmp_port": "25462",
-    "timezone": "Europe/London",
-    "time_now": "2026-04-14 12:12:26"
+    "rtmp_port": "8880",
+    "timestamp_now": 1776388311,
+    "timezone": "America/Sao_Paulo"
   }
 }
