1da49fbc89f6a0095f40bcf660f3dbaba518e17b
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-19 21:15:41 -0400

    Update meuTV.php

--- meuTV.php
+++ meuTV.php
@@ -1,7 +1,14 @@
 {
   "user_info": {
-    "username": "40937276",
-    "password": "67063536",
+    "username": "102030",
+    "password": "102030",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": null,
+    "is_trial": "0",
+    "active_cons": null,
+    "created_at": "1776381551",
+    "max_connections": "null",
     "allowed_output_formats": [
       "m3u8",
       "ts",
@@ -9,7 +16,10 @@
     ]
   },
   "server_info": {
-    "url": "206.109.57.241",
+    "xui": true,
+    "version": "1.5.13",
+    "revision": null,
+    "url": "zyxonline.online",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
