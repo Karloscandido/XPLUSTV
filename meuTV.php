788205b7de23872cd4b3fad989c9892e5530ec7d
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-19 22:48:11 -0400

    Update meuTV.php

--- meuTV.php
+++ meuTV.php
@@ -1,31 +1,27 @@
 {
   "user_info": {
-    "username": "tanaka",
-    "password": "302718",
-    "message": "666",
+    "username": "2567966723",
+    "password": "6979752239",
+    "message": "null",
     "auth": 1,
     "status": "Active",
-    "exp_date": "1838672159",
+    "exp_date": "1802197232",
     "is_trial": "0",
     "active_cons": "0",
-    "created_at": "1773123561",
+    "created_at": "1687458326",
     "max_connections": "0",
     "allowed_output_formats": [
       "m3u8",
-      "ts",
-      "rtmp"
+      "ts"
     ]
   },
   "server_info": {
-    "xui": true,
-    "version": "1.5.13",
-    "revision": null,
-    "url": "topflx.online",
-    "port": "80",
-    "https_port": "443",
+    "url": "ultrax.online",
+    "port": "8080",
+    "https_port": "2083",
     "server_protocol": "http",
-    "rtmp_port": "8880",
-    "timestamp_now": 1776649943,
-    "timezone": "America/Manaus"
+    "rtmp_port": "2053",
+    "timezone": "America/Santiago",
+    "timestamp_now": 1776653159,
   }
 }
