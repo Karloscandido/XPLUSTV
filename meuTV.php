a518ebb307e1ab2bd01c24983417d80665a9c36c
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-19 21:02:45 -0400

    Update meuTV.php

--- meuTV.php
+++ meuTV.php
@@ -2,13 +2,6 @@
   "user_info": {
     "username": "40937276",
     "password": "67063536",
-    "message": "Bem vindo(a) !",
-    "auth": 1,
-    "status": "Active",
-    "exp_date": null,
-    "is_trial": "0",
-    "active_cons": 1,
-    "max_connections": "200000",
     "allowed_output_formats": [
       "m3u8",
       "ts",
@@ -16,16 +9,10 @@
     ]
   },
   "server_info": {
-    "xui": true,
-    "version": "1.5.5",
-    "revision": 2,
     "url": "206.109.57.241",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
-    "timestamp_now": 1776568801,
-    "time_now": "2026-04-19 03:20:01",
-    "timezone": "UTC"
   }
 }
